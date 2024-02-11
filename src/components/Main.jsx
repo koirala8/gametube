0b1118df551e1daadf1b79000b3b5719ffd6a391
Mahendra Koirala <60951574+koirala8@users.noreply.github.com>
2024-02-11 02:37:11 -0600

    Fast Scrolling while browsing games is added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -37,7 +37,7 @@ const Main = () => {
         <div className="absolute w-full top-[20%] p-4 md:p-8">
           <h1 className="text-3xl font-bold text-white md:text-5xl">{game?.title}</h1>
           <div className="my-4">
-            <button className="px-5 py-2 text-black border border-gray-300 rounded-md bg-zinc-400">Play</button>
+            <button className="px-5 py-2 text-black border border-gray-300 rounded-md bg-zinc-200">Play</button>
             <button className="px-5 py-2 ml-4 text-white border border-gray-300 rounded-md">Play Later</button>
           </div>
           <p className="text-sm text-white">Released: {game?.release_date}</p>
