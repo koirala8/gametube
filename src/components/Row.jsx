9d95ca830f5b5f1b3077266b880b946d5ed21570
koirala8 <60951574+koirala8@users.noreply.github.com>
2024-02-15 15:02:55 -0600

    overiding

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-export const Row = ({  getData }) => {
+export const Row = ({ getData }) => {
   const [games, setGames] = useState([]);
-  const [pcGames, setPcGames] = useState([]);
-  
 
   useEffect(() => {
     const fetchData = async () => {
@@ -12,12 +10,6 @@ export const Row = ({  getData }) => {
         // Fetch data for all platforms
         const gameData = await getData();
         setGames(gameData);
-
-        // Fetch data for PC platform
-        const pcData = await getData('pc');
-        setPcGames(pcData);
-
-       
       } catch (error) {
         console.error(error);
       }
@@ -26,39 +18,22 @@ export const Row = ({  getData }) => {
     fetchData();
   }, [getData]);
 
-  // Render the component
-
-
   return (
-    <div>
-      <>
-        <div className="relative flex items-center">
-            
-          <div id={"slider"}>
-
-            {games.map((item, id) => (
-              <div
-                key={id}
-                className="w-[160px] sm:w-[200px] md:w-[240px] ml-6 lg:w-[280px] inline-block cursor-pointer p-2"
-              >
-                <img src={item?.thumbnail} alt={item?.title} />
+    <div className="relative items-center p-2">
+      <div id="slider">
+        <ul className="flex flex-row gap-4 overflow-x-auto p-0 m-0">
+          {games.map((item, id) => (
+            <li key={id} className="relative cursor-pointer p-2">
+              <div className="w-[160px] sm:w-[200px] md:w-[240px] ml-6 lg:w-[280px] relative">
+                <img className="w-full h-full block" src={item?.thumbnail} alt={item?.title} />
+                <div className="absolute top-0 left-0 w-full h-full hover:bg-black/60 opacity-0 hover:opacity-100 text-white">
+                  <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center h-full text-center items-center">{item?.title}</p>
+                </div>
               </div>
-            ))}
-
-            {pcGames.map((item, id) => (
-              <div
-                key={id}
-                className="w-[160px] sm:w-[200px] md:w-[240px] ml-6 lg:w-[280px] inline-block cursor-pointer p-2"
-              >
-                <img src={item?.thumbnail} alt={item?.title} />
-              </div>
-            ))}
-
-            
-            
-          </div>
-        </div>
-      </>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
