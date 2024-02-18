8aa33601717b237c6dc34b216ede862d3ad95ead
Mahendra Koirala <60951574+koirala8@users.noreply.github.com>
2024-02-17 23:14:56 -0600

    Used cloud firebase database to store user's favorite games

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -1,9 +1,33 @@
 import React, { useState } from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { UserAuth } from "../context/AuthContext";
+import { db } from "../firebase";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
 
 export const Games = ({ item, id }) => {
     const [like, setLike] = useState(false);
-  
+    const [saved, setSaved] = useState(false);
+    const {user} = UserAuth();
+
+    const gameID = doc(db, 'users', `${user?.email}`);
+
+    const savedGames = async () => {
+      if (user?.email) {
+        setLike(!like)
+        setSaved(true)
+
+        await updateDoc(gameID, {
+          savedGames: arrayUnion({
+            id: item.id,
+            title: item.title,
+            img: item.thumbnail
+          })
+        })
+      }
+      else{
+        alert('Please log in to save a game');
+      }
+    }
     return (
       <div
         key={id}
@@ -19,7 +43,7 @@ export const Games = ({ item, id }) => {
             <p className="flex items-center justify-center h-full text-xs font-bold text-center text-white white-space-normal md:text-sm">
               {item?.title}
             </p>
-            <p>
+            <p onClick={savedGames}>
               {like ? (
                 <FaHeart className="absolute text-gray-300 top-4 left-4" />
               ) : (
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,20 +1,28 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { auth, db } from '../firebase';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
-import {setDoc, doc} from "firebase/firestore";
+import {doc, setDoc} from "firebase/firestore";
 
 const AuthContext = createContext();
 
 export function AuthContextProvider({ children }) {
     const [user, setUser] = useState({});
 
-    function signUp(email, password) {
-
-        createUserWithEmailAndPassword(auth, email, password);
-        setDoc(doc(db, 'users', email), {
-            savedShows: []
+    async function signUp(email, password) {
+        try {
+            // Create user account
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            
+            // Add user to Firestore database
+            await setDoc(doc(db, 'users', email), {
+                savedGames: []
+            });
+        } catch (error) {
+            console.log(error);
+            // Handle error
         }
-    )}
+    }
+    
 
     function logIn(email, password) {
         return signInWithEmailAndPassword(auth, email, password);
