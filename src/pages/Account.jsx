f65502b073818b63549c831c8ad98ca0bcebdd72
Mahendra Koirala <60951574+koirala8@users.noreply.github.com>
2024-02-22 23:03:15 -0600

    Setup Account page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Account = () => {
   return (
-    <div className='text-center bg-black'>Account</div>
+    <div className='text-center text-white bg-black'>Account</div>
   )
 }
